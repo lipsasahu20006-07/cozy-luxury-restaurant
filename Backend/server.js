febc6b3c4d9c716bf081f70e44b9ae1c0e915c73
lipsa05 <lipsawork989@gmail.com>
2026-08-09 19:42:18 +0530

    Remove MongoDB URI from server logs

--- Backend/server.js
+++ Backend/server.js
@@ -5,35 +5,19 @@ const dotenv = require("dotenv");
 const reservationRoutes = require("./routes/reservationRoutes");
 
 dotenv.config();
-console.log("MONGO_URI =", process.env.MONGO_URI);
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+
 app.use("/api", reservationRoutes);
-
-
-async function startServer() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("✅ Connected to MongoDB");
-
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err);
-  }
-}
-
-
-
-const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   try {
